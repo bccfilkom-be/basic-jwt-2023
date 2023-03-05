f9655f62feb3a2419c1247896ca3a4bc562954f8
Muh Zaqi <zaqiikhwan22@gmail.com>
2023-03-05 12:19:50 +0700

    feat: todo repository

--- repository/todo.go
+++ repository/todo.go
@@ -12,7 +12,7 @@ type (
 	TodoRepository interface {
 		Create(todo *domain.Todo) error
 		FindById(id uint) (*domain.Todo, error)
-		FindAllByIdUser(idUser uint) ([]*domain.Todo, error)
+		FindAllByIdTodo(idTodo uint) ([]*domain.Todo, error)
 		Delete(id uint) error
 		Update(todo *domain.Todo) error
 	}
@@ -22,40 +22,40 @@ func NewTodoRepository(db *gorm.DB) TodoRepository {
 	return &todoRepository{db: db}
 }
 
-func(ur *userRepository) Create(todo *domain.User) error {
-	if err := ur.db.Create(&user).Error; err != nil {
+func(tr *todoRepository) Create(todo *domain.Todo) error {
+	if err := tr.db.Create(&todo).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func(ur *userRepository) FindById(idUser uint) (*domain.User, error) {
-	var user domain.User
-	if err := ur.db.First(&user, idUser).Error; err != nil {
+func(tr *todoRepository) FindById(idTodo uint) (*domain.Todo, error) {
+	var todo *domain.Todo
+	if err := tr.db.First(&todo, idTodo).Error; err != nil {
 		return nil, err
 	}
-	return &user, nil
+	return todo, nil
 }
 
-func(ur *userRepository) FindAllByIdUser(idUser uint) (*[]domain.User, error) {
-	var users []domain.User
-	if err := ur.db.Find(&users, idUser).Error; err != nil {
+func(tr *todoRepository) FindAllByIdTodo(idTodo uint) ([]*domain.Todo, error) {
+	var todos []*domain.Todo
+	if err := tr.db.Find(&todos, idTodo).Error; err != nil {
 		return nil, err
 	}
-	return &users, nil
+	return todos, nil
 }
 
-func(ur *userRepository) Delete(id uint) error {
-	var user domain.User
-	if err := ur.db.Delete(&user, id).Error; err != nil {
+func(tr *todoRepository) Delete(id uint) error {
+	var todo domain.Todo
+	if err := tr.db.Delete(&todo, id).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (ur *userRepository) Update(user *domain.User) error {
-	var updateUser domain.User
-	if err := ur.db.Model(&updateUser).Updates(user).Error; err != nil {
+func (tr *todoRepository) Update(todo *domain.Todo) error {
+	var updateTodo domain.Todo
+	if err := tr.db.Model(&updateTodo).Updates(todo).Error; err != nil {
 		return err
 	}
 	return nil
